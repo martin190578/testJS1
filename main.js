fda53d2844477b82f03c75ca8874dc476e8e1d37
ahmadHadizadeh <giv.m@yahoo.com>
2022-06-25 11:36:13 +0430

    'stracherjoin'

---
+++ main.js
@@ -0,0 +1,4 @@
+var names = ['ahmad', 'saman', 'ali', 'reza', 'shima'];
+
+document.getElementById('p1').innerHTML = names;
+document.getElementById('p1').innerHTML = names.join('*');
---
+++ var names = ['ahmad', 'saman', 'ali', 'r.js
@@ -0,0 +1,4 @@
+var names = ['ahmad', 'saman', 'ali', 'reza', 'shima'];
+
+// console.log(names);
+document.getElementById('p1').innerHTML = names;
